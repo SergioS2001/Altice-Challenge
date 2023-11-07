4abb0ba13597247cac12ff8bd3bd918c4d3f4ebb
SergioS2001 <106494057+SergioSousa39836@users.noreply.github.com>
2023-11-07 15:25:46 +0000

    ChargeRequest e Calculo de billing (por terminar)

--- src/BillingAccount.java
+++ src/BillingAccount.java
@@ -1,6 +1,8 @@
+import java.sql.Timestamp;
 import java.util.Date;
+import java.time.LocalDateTime;
 
-public class BillingAccount {
+public class BillingAccount{
     String MSISDN;
     int Bucket1;
     int Bucket2;
@@ -10,10 +12,13 @@ public class BillingAccount {
     int CounterB;
     int CounterC;
     Date CounterD;
+
     String [] TarifarioServicoA = {"Alpha1", "Alpha2", "Alpha3"};
+
     String [] TarifarioServicoB = {"Beta1", "Beta2", "Beta3"};
 
     public BillingAccount(String MSISDN, int bucket1, int bucket2, int bucket3, int counterA, int counterB, int counterC, Date counterD) {
+        super();
         this.MSISDN = MSISDN;
         Bucket1 = bucket1;
         Bucket2 = bucket2;
@@ -26,13 +31,202 @@ public class BillingAccount {
       //  TarifarioServicoB = tarifarioServicoB;
     }
 
-    public static void main(String[] args) {
-
-    }
 
     /**2 isso vai passar para a billing account, recebe essa instancia e vai fazer as suas verificacoes e ver em que tipo de servico, dentro do A ou B ele vai ficar**/
 
-     public void VerificationOfService(ChargingRequest request){}
+     public void VerificationOfService(ChargingRequest request){
+         char Service = request.Service;
+
+         if (Service == 'A'){
+             ServiceA(request);
+         }
+         else{
+             ServiceB(request);
+         }
+     }
+
+    /**
+     *
+     * counterA - contagem de SU's do Serviço A;
+     * o counterB - contagem do numero de pedidos do Serviço B, sob tarifário Beta1;
+     * o counterC- contagem de requisições em roaming
+     * o counterD- registro da data da ultima requisição feita
+     */
+
+     public int ServiceA(ChargingRequest request){
+         String MSISND = request.MSISDN;
+         CounterA = request.RSU;
+         Timestamp time = request.TimeStamp;
+         LocalDateTime LocalTime = time.toLocalDateTime();
+         int DayOfWeek = LocalTime.getDayOfWeek().getValue();
+         int Hours = LocalTime.getHour();
+         boolean Roaming = request.Roaming;
+
+         //alpha 1
+         if(CounterA < 100){
+             //if para verificar que so da aos dias da semana
+             if(DayOfWeek >= 1 && DayOfWeek<=5){
+
+                 if (Roaming){
+                     CounterC+=2;
+                 }
+                 if (Hours >= 20 || Hours <= 6){
+                     CounterA +=0.5;
+                 }
+
+                 CounterA += 1;
+
+                 if(CounterA < 10){
+                     CounterA-=0.25;
+                 }
+                 if (CounterC<50){
+                     CounterC -=0.1;
+                 }
+
+                 System.out.println("The type of Service is Alpha 1");
+                 System.out.println("The bucket that will be charged");
+
+                  if (Roaming){
+                      System.out.println("Bucket B");
+                  }
+                  else if(Roaming && CounterC > 5){
+                      System.out.println("Bucket B");
+                  }
+                  else {
+                      System.out.println("Bucket A");
+                  }
+             }
+         }
+         //alpha2
+         if(Bucket2 < 10 || Roaming){
+             System.out.println("Invalid for Alpha 2");
+             return -1;
+         }
+         else{
+             if (Hours >= 20 || Hours <= 6){
+                 CounterA +=0.25;
+             }
+             if (CounterB > 10){
+                 CounterA -=0.5;
+             }
+             if (Bucket2 > 15){
+                 CounterA -=0.05;
+             }
+             CounterA+=0.5;
+             //verificar como posso fazer para que o pagamento seja apenas no bucket2
+             System.out.println("Value charged in Bucket2");
+         }
+
+         //alpha3
+         if(Roaming && Bucket3 > 10){
+
+             if(DayOfWeek > 5 && DayOfWeek < 7){
+                 CounterA += 0.25;
+             }
+             if(CounterB > 10){
+                 CounterA -=0.2;
+             }
+             if(CounterB > 15){
+                 CounterA -=0.05;
+             }
+             //pagamento apenas no bucket3
+         }
+
+         //retornar o pagamento
+         return 0;
+     }
 
 
+    /**
+     *
+     * counterA - contagem de SU's do Serviço A;
+     * o counterB - contagem do numero de pedidos do Serviço B, sob tarifário Beta1;
+     * o counterC- contagem de requisições em roaming
+     * o counterD- registro da data da ultima requisição feita
+     */
+
+
+     public int ServiceB(ChargingRequest request){
+         String MSISND = request.MSISDN;
+         CounterA = request.RSU;
+         Timestamp time = request.TimeStamp;
+         LocalDateTime LocalTime = time.toLocalDateTime();
+         int DayOfWeek = LocalTime.getDayOfWeek().getValue();
+         int Hours = LocalTime.getHour();
+         boolean Roaming = request.Roaming;
+
+         //beta 1
+         if(DayOfWeek >= 1 && DayOfWeek<=5 || DayOfWeek >= 6 && Hours >=20 && Hours <= 6)
+         {
+
+             if (Roaming){
+                 CounterC+=0.2;
+             }
+             if(Hours >= 20 && Hours <= 6){
+                 CounterB+=0.05;
+             }
+             CounterB+=0.1;
+
+             if(CounterA > 10){
+                 CounterB-=0.025;
+             }
+             if(Bucket3 > 50){
+                 CounterB-=0.01;
+             }
+             if (Roaming){
+                 System.out.println("Debit in bucket3");
+             }
+             if(Roaming && CounterB>5){
+                 System.out.println("Debit in bucket2");
+             }
+             System.out.println("Debit in bucketA");
+         }
+
+         //beta2
+         if(Roaming || Bucket2 > 10) {
+             System.out.println("Invalid for beta2");
+         }else{
+
+             if(Hours >= 20 && Hours <= 6){
+                 CounterB+=0.025;
+             }
+             CounterB+=0.05;
+
+             if(CounterB > 10){
+                 CounterB-=0.02;
+             }
+             if(Bucket2 > 15){
+                 CounterB-=0.05;
+             }
+             //apenas pode debitar no bucket2 ou rejeita
+             System.out.println("Debit in bucketB");
+         }
+
+         //beta3
+         if(Roaming == false || Bucket3 > 10) {
+             System.out.println("Invalid for beta2");
+         }else{
+
+             if(Hours >= 20 && Hours <= 6){
+                 CounterB+=0.025;
+             }
+             CounterB+=0.1;
+
+             if(CounterB > 10){
+                 CounterB-=0.02;
+             }
+             if(Bucket2 > 15){
+                 CounterB-=0.05;
+             }
+             //apenas pode debitar no bucket3 ou rejeita
+             System.out.println("Debit in bucketC");
+         }
+
+        //no fim retornar um valor de pagamento
+        return 0;
+     }
+
+    public static void main(String[] args) {
+
+    }
 }
--- src/ChargingRequest.java
+++ src/ChargingRequest.java
@@ -1,4 +1,7 @@
 import java.sql.Timestamp;
+import java.util.Date;
+import java.util.Scanner;
+import java.util.UUID;
 
 public class ChargingRequest {
     String RequestID;
@@ -18,10 +21,50 @@ public class ChargingRequest {
         this.RSU = RSU;
     }
 
-    public static void main(String[] args) {
-        /**1 criar uma instancia de uma charging request**/
+         public static ChargingRequest CreateNewChargingRequest(){
 
+             Scanner scan = new Scanner(System.in);
+             String RequestID;
+             char Service;
+             boolean Roaming;
+             String PhoneNumber;
+             int RSU;
+             Date date;
+             Timestamp Timestamp;
+
+             // Generate a unique RequestID
+             RequestID = UUID.randomUUID().toString();
+
+             System.out.println("Please enter your phone number: ");
+             PhoneNumber = scan.nextLine();
+
+             System.out.println("Enter type of Service (A or B): ");
+             Service = scan.next().charAt(0);
+
+             System.out.println("How many Service Units?: ");
+             RSU = scan.nextInt();
+
+             System.out.println("Will you use roaming? (True or False): ");
+             Roaming = scan.hasNextBoolean();
+
+             Timestamp = new Timestamp(System.currentTimeMillis());
+
+             System.out.println("Charging Request create successfully\n\n");
+
+             return new ChargingRequest(RequestID, Timestamp, Service, Roaming, PhoneNumber, RSU);
 
          }
 
+
+    public static void main(String[] args) {
+        ChargingRequest newRequest = CreateNewChargingRequest();
+       // System.out.println(newRequest.toString());
+        System.out.println("New Charging Request created:");
+        System.out.println("RequestID: " + newRequest.RequestID);
+        System.out.println("Timestamp: " + newRequest.TimeStamp);
+        System.out.println("Service: " + newRequest.Service);
+        System.out.println("Roaming: " + newRequest.Roaming);
+        System.out.println("MSISDN: " + newRequest.MSISDN);
+        System.out.println("RSU: " + newRequest.RSU);
+    }
 }
